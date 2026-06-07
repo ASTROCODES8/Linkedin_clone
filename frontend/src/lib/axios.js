3be98902e251909b2340a5ce3091f41daf764901
ASTROCODES8 <funbliss8888@gmail.com>
2026-06-08 00:01:24 +0530

    fix api url for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
   withCredentials: true,
 });
