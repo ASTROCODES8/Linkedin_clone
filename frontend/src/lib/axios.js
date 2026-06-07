229280074f7e6445f8c9a039ac8d4f06a5a71aba
ASTROCODES8 <funbliss8888@gmail.com>
2026-06-08 00:12:14 +0530

    a fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL: "https://linkedin-clone-nine-sigma.vercel.app/api/v1",
   withCredentials: true,
 });
